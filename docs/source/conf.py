5f158ad35144151b7f04ec2ee0ece74b35c91670
remi-boivin <remi.boivin@epitech.eu>
2023-05-31 07:03:43 +0200

    :tada: chore: Initial commit
    add project structure and common files

---
+++ docs/source/conf.py
@@ -0,0 +1,45 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+
+import os
+import sys
+import pathlib
+
+sys.path.insert(0, os.path.abspath('../sample'))
+path = pathlib.Path(__file__).resolve() / '..' / '..' / '..' / 'sample'
+
+# sys.path.insert(0, os.path.abspath('..\src'))
+sys.path.insert(0, os.path.abspath(path))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Python game prototype'
+copyright = '2023, Rémi Boivin'
+author = 'Rémi Boivin'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
